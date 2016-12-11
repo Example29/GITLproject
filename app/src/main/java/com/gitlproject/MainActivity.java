d403e263d08b517f4a89c976fbd8be26ac133d41
Latha <latha@monkeybox.in>
2016-12-11 17:02:22 +0530

    firstcommit

---
+++ app/src/main/java/com/gitlproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gitlproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
